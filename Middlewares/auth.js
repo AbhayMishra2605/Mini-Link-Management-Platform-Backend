0385b6c3e5be7c1f902a212a6ab3a6f9bcd1de96
AbhayMishra2605 <152012187+AbhayMishra2605@users.noreply.github.com>
2025-01-31 15:56:34 +0530

    Update auth.js

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,17 +1,27 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const User=require("../Schema/user.Schema");
+
 dotenv.config();
-const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization;  // check for auth token
+const authMiddleware =async(req, res, next) => {
+    const token = req.headers.authorization;  
     if (!token) {
         return res.status(401).json({ message: "This action is not allowed" });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+       
+
+          if (user.tokenIssuedAt > decoded.tokenIssuedAt) {
+            return res.status(401).json({ message: "Session expired, please log in again" });
+        }
+        
         req.user = decoded;
         
         next();
     } catch (err) {
+        console.log(err);
         res.status(500).json({ message: "Invalid token" });
     }
 };
